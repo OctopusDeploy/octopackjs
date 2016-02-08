7c45814b35c63eca2d62f46864d1d9bb5cd82eaa
Marshall Bowers <mbowers@gravic.com>
2016-02-08 12:45:25 -0500

    Lessen scope of `req`

--- test.js
+++ test.js
@@ -31,30 +31,28 @@ describe('push', function() {
   });
 
   describe('build url', function () {
-    var req;
-
     it('should include `replace` parameter if it is provided', function () {
       octo.push(new Buffer('hello world'), { replace: true, host: 'http://myweb/' });
-      req = postStub.firstCall.args[0];
+      var req = postStub.firstCall.args[0];
       expect(req.url).to.equal('http://myweb/api/packages/raw?replace=true');
       postStub.reset();
     });
 
     it('should build correct url', function () {
       octo.push(new Buffer('hello world'), { host: 'http://myweb' });
-      req = postStub.firstCall.args[0];
+      var req = postStub.firstCall.args[0];
       expect(req.url).to.equal('http://myweb/api/packages/raw');
     });
 
     it('should build correct url with port', function () {
       octo.push(new Buffer('hello world'), { host: 'http://myweb:3000' });
-      req = postStub.firstCall.args[0];
+      var req = postStub.firstCall.args[0];
       expect(req.url).to.equal('http://myweb:3000/api/packages/raw');
     });
 
     it('should build correct url with relative path', function () {
       octo.push(new Buffer('hello world'), { host: 'http://myweb/path/to/octopus/' });
-      req = postStub.firstCall.args[0];
+      var req = postStub.firstCall.args[0];
       expect(req.url).to.equal('http://myweb/path/to/octopus/api/packages/raw');
     });
   });
